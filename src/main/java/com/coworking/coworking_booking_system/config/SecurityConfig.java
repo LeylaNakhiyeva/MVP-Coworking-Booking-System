e60be586222b14c81b57fe9942765acc3e613cbf
Mustafa1986 <m.aliyevs@yandex.ru>
2026-04-13 10:09:02 +0400

    Document API requirements and security notes in SecurityConfig
    Added comments outlining required API endpoints and security considerations.

--- src/main/java/com/coworking/coworking_booking_system/config/SecurityConfig.java
+++ src/main/java/com/coworking/coworking_booking_system/config/SecurityConfig.java
@@ -22,3 +22,38 @@
 //        return http.build();
 //    }
 //}
+
+/*
+sene verdiyim MVP de bu metodlar olmalidi  
+GET /api/workspaces
+GET /api/workspaces/available?date=...
+POST /api/bookings
+GET /api/bookings/my
+DELETE /api/bookings/{id}
+ADMIN: POST /api/workspaces
+ADMIN: PUT /api/workspaces/{id}
+ADMIN: GET /api/bookings 
+
+sen ise ancaq bunlari yaradibsan 
+GET /api/workspaces
+GET /api/workspaces/available
+POST /api/bookings
+GET /api/bookings/user/{userId} 
+
+
+Bunlar yoxdu 
+GET /api/bookings/my
+DELETE /api/bookings/{id}
+POST /api/workspaces
+PUT /api/workspaces/{id}
+GET /api/bookings 
+
+ 
+
+SecurityConfig.java hamisi kommentlenib 
+Basic Auth 
+USER / ADMIN icazeler 
+passwordlar xeshlenmir 
+Mvp de men yazmisdim ki Basic Auth BCrypt olsun hec olmasa 
+
+*/
